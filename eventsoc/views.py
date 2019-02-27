e7652e8d66aee990067aeea5a9a2da74faf45c62
Hugh Carslaw <2314197c@student.gla.ac.uk>
2019-02-27 17:40:55 +0000

    Register and login views done

--- eventsoc/views.py
+++ eventsoc/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import permission_required, login_required
+
+# Permissions will be defined in models
+
 # Create your views here.
 
 
@@ -11,26 +15,59 @@ def login(request):
     return render(request, "eventsoc/login.html", {})
 
 
+@login_required
+@permission_required(eventsoc.is_society)
 def create_event(request):
     return render(request, "eventsoc/create_event.html", {})
 
 
 def register(request):
-    return render(request, "eventsoc/register.html", {})
+    registered = False
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        society_form = SocietyForm(data=request.POST)
+        if user_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            registered = True
+        elif user_form.is_valid() == False:
+            print(user_form.errors)
 
+        if society_form.is_valid:
+            society = society_form.save()
+            society.set_password(society.password)
+            society.save()
+            registered = True
+        elif society_form.is_valid == False:
+            print(society_form.errors)
+    else:
+        user_form = UserForm()
+        society_form = SocietyForm()
+    return render(request, 'wad_project/register.html',
+                {'user_form': user_form,
+                'society_form': society_form,
+                'registered': registered})
 
+@login_required
+@permission_required(eventsoc.is_society)
 def edit_event(request):
     return render(request, "eventsoc/edit_event.html", {})
 
 
+@login_required
 def edit_profile(request):
     return render(request, "eventsoc/edit_profile.html", {})
 
 
+@login_required
+@permission_required(eventsoc.is_user)
 def booked(request):
     return render(request, "eventsoc/booked.html", {})
 
 
+@login_required
+@permission_required(eventsoc.is_user)
 def account(request):
     return render(request, "eventsoc/account.html", {})
 
@@ -39,9 +76,13 @@ def society(request):
     return render(request, "eventsoc/society.html", {})
 
 
+@login_required
+@permission_required(eventsoc.society)
 def past_events(request):
     return render(request, "eventsoc/past_events.html", {})
 
 
+@login_required
 def user_logout(request):
-    return render(request, "eventsoc/user_logout.html", {})
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
