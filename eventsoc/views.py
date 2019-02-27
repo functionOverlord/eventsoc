367f01b833fcf1f1865317f2264c5bd9ef46c853
Leo Kumagai <2314838K@student.gla.ac.uk>
2019-02-27 16:36:31 +0000

    Added template in settings.py, mock views.py to match urls.py

--- eventsoc/views.py
+++ eventsoc/views.py
@@ -4,8 +4,32 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello world!")
 
+def login(request):
+    return HttpResponse("Login")
+
 def create_event(request):
     return HttpResponse("Create events page")
 
-def login(request):
-    return HttpResponse("Login")
+def register(request):
+    return HttpResponse("register")
+
+def edit_event(request):
+    return HttpResponse("edit_event")
+
+def edit_profile(request):
+    return HttpResponse("edit_profile")
+
+def booked(request):
+    return HttpResponse("booked")
+
+def account(request):
+    return HttpResponse("account")
+
+def society(request):
+    return HttpResponse("society")
+
+def past_events(request):
+    return HttpResponse("past_events")
+
+def user_logout(request):
+    return HttpResponse("user_logout")
