ca06ea8834bde6db782e32a1a965669f3dcdb65d
Leo Kumagai <2314838K@student.gla.ac.uk>
2019-02-27 16:21:27 +0000

    urls patterns added, installed app change to settings, views

--- eventsoc/views.py
+++ eventsoc/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello world!")
+
+def create_event(request):
+    return HttpResponse("Create events page")
+
+def login(request):
+    return HttpResponse("Login")
