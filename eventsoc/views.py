5dfa219423e4d65d950b0cfb0c110d1a625581bc
mario <Mario.L.Geuenich@gmail.com>
2019-02-28 12:26:43 +0000

    added bootstrap support, removed dir in wrong place

--- eventsoc/views.py
+++ eventsoc/views.py
@@ -1,35 +1,47 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
+
 def index(request):
-    return HttpResponse("Hello world!")
+    return render(request, "eventsoc/index.html", {})
+
 
 def login(request):
-    return HttpResponse("Login")
+    return render(request, "eventsoc/login.html", {})
+
 
 def create_event(request):
-    return HttpResponse("Create events page")
+    return render(request, "eventsoc/create_event.html", {})
+
 
 def register(request):
-    return HttpResponse("register")
+    return render(request, "eventsoc/register.html", {})
+
 
 def edit_event(request):
-    return HttpResponse("edit_event")
+    return render(request, "eventsoc/edit_event.html", {})
+
 
 def edit_profile(request):
-    return HttpResponse("edit_profile")
+    return render(request, "eventsoc/edit_profile.html", {})
+
 
 def booked(request):
-    return HttpResponse("booked")
+    return render(request, "eventsoc/booked.html", {})
+
 
 def account(request):
-    return HttpResponse("account")
+    return render(request, "eventsoc/account.html", {})
+
 
 def society(request):
-    return HttpResponse("society")
+    return render(request, "eventsoc/society.html", {})
+
 
 def past_events(request):
-    return HttpResponse("past_events")
+    return render(request, "eventsoc/past_events.html", {})
+
 
 def user_logout(request):
-    return HttpResponse("user_logout")
+    return render(request, "eventsoc/user_logout.html", {})
