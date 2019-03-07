fc83c9d7fedd09e06608937cb6ad12933c0ca3f1
Hugh Carslaw <2314197c@student.gla.ac.uk>
2019-03-07 12:40:16 +0000

    admin and media

--- eventsoc/admin.py
+++ eventsoc/admin.py
@@ -1,10 +1,4 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from eventsoc.models import NewUser
 # Register your models here.
 admin.site.register(NewUser)
-=======
-from eventsoc.models import UserProfile
-# Register your models here.
-admin.site.register(UserProfile)
->>>>>>> a5fe824... Added user registration
