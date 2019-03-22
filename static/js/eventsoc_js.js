efdcc4996a17343e7a91ec7677db499ae8ce2839
Mario Geuenich <Mario.L.Geuenich@gmail.com>
2019-03-22 16:20:19 +0000

    center register forms

--- static/js/eventsoc_js.js
+++ static/js/eventsoc_js.js
@@ -3,13 +3,11 @@ $(function () {
     $(".btn-register-user").click( function () {
         $("#user-form").css("display", "inline");
         $(".register-buttons").hide();
-        $("#header").hide();
     });
 
     $(".btn-register-society").click( function () {
         $("#society-form").css("display", "inline");
         $(".register-buttons").hide();
-        $("#header").hide();
     });
 
     $("#about-btn").click( function(event) {
